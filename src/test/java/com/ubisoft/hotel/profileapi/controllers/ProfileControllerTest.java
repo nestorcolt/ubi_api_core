15fae207aae2841f527517ada0a74b539c617525
nestorcolt <nestor.colt@gmail.com>
2021-08-21 17:05:38 +0200

    unit test init

---
+++ src/test/java/com/ubisoft/hotel/profileapi/controllers/ProfileControllerTest.java
@@ -0,0 +1,36 @@
+package com.ubisoft.hotel.profileapi.controllers;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class ProfileControllerTest {
+
+    @Test
+    @DisplayName("UNIT - Info API health >>")
+    void info() {
+
+        Assertions.assertEquals(0,0);
+    }
+
+//    @Test
+//    void createProfile() {
+//    }
+//
+//    @Test
+//    void updateProfile() {
+//    }
+//
+//    @Test
+//    void getAllProfiles() {
+//    }
+//
+//    @Test
+//    void getProfile() {
+//    }
+//
+//    @Test
+//    void removeProfile() {
+//    }
+}
