dfe207cbf2d3a14a450c68e724bb53db586de5a4
nestorcolt <nestor.colt@gmail.com>
2021-08-21 17:18:34 +0200

    unit test file for api profile controller squeleton done

--- src/test/java/com/ubisoft/hotel/profileapi/controllers/ProfileControllerTest.java
+++ src/test/java/com/ubisoft/hotel/profileapi/controllers/ProfileControllerTest.java
@@ -6,31 +6,67 @@ import org.junit.jupiter.api.Test;
 
 
 class ProfileControllerTest {
+    /*
+    Happy path test suite for the API profile controller
+     */
 
     @Test
     @DisplayName("UNIT - Info API health >>")
     void info() {
+        // Arrange
 
-        Assertions.assertEquals(0,0);
+        // Act
+
+        // Assert
     }
 
-//    @Test
-//    void createProfile() {
-//    }
-//
-//    @Test
-//    void updateProfile() {
-//    }
-//
-//    @Test
-//    void getAllProfiles() {
-//    }
-//
-//    @Test
-//    void getProfile() {
-//    }
-//
-//    @Test
-//    void removeProfile() {
-//    }
+    @Test
+    @DisplayName("UNIT - Create a profile >>")
+    void createProfile() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    @DisplayName("UNIT - Updates a profile >>")
+    void updateProfile() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    @DisplayName("UNIT - Get all profiles >>")
+    void getAllProfiles() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    @DisplayName("UNIT - Get single profile >>")
+    void getProfile() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    @Test
+    @DisplayName("UNIT - Remove single profile >>")
+    void removeProfile() {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
 }
